493f51dac35069166e02b569cfc3503eba3f5ad3
Juan Jose Atuesta Flor <juanjoseatuestaflor@gmail.com>
2026-02-26 21:25:46 -0500

    pequeñas mejoras

--- src/main/scala/Recursion/package.scala
+++ src/main/scala/Recursion/package.scala
@@ -6,19 +6,19 @@ package object Recursion {
     if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
-  /* 
-    * Ejercicio 3: balanceado
-    * */
+    /* 
+     * Ejercicio 3: balanceado
+     * */
   def balanceado(chars: List [Char]):Boolean = {
     def balanceadoAux(chars: List[Char], count: Int):Boolean ={
-     if(count != 0 && chars.isEmpty) false else true
+     if(count != 0) false else true
     }
 
       def validar(chars: List[Char], count: Int): Boolean = {
       if(chars.isEmpty) balanceadoAux(chars, count)
       else if(chars.head == '(') validar(chars.tail, count + 1)
       else if(chars.head == ')' && count > 0) validar(chars.tail, count - 1)
-      else if (chars.head == ')' && count <= 0) false
+      else if (chars.head == ')' && count == 0) false
       else validar(chars.tail, count)
     }
       validar(chars, 0)
