47d37090723841870e22f0117b044aeed078f420
Juan Jose Atuesta Flor <juanjoseatuestaflor@gmail.com>
2026-03-04 00:57:09 -0500

    quite una funcion extra que no era necesaria en mi funcion

--- .scala-build/taller1_dfcb79a7c1/src_generated/main/src/main/scala/Recursion/pruebas.scala
+++ .scala-build/taller1_dfcb79a7c1/src_generated/main/src/main/scala/Recursion/pruebas.scala
@@ -7,7 +7,6 @@ def scriptPath = """src/main/scala/Recursion/pruebas.sc"""
 /*<script>*/
 import Recursion.{potencia, pascal, balanceado, cambioMonedas}
 
-
 //Pruebas potencia
 println(potencia(2, 3))
 println(potencia(5, 0))
@@ -19,30 +18,18 @@ println(pascal(1, 1))
 println(pascal(2, 3))
 
 //Pruebas balanceado de parentesis
-println( balanceado("(hola) como estas? ())(".toList) )
-println( balanceado("(hola) como estas? ()".toList) ) 
-println( balanceado("(hola) como estas? ()(".toList) ) 
-println( balanceado("(hola) como estas? ())".toList) ) 
+println(balanceado("(hola) )(como estas? ()".toList))
+println(balanceado("(hola) como estas? ()".toList))
+println(balanceado("(hola) como (estas? ()".toList))
+println(balanceado("(hola) como estas? ())".toList))
+println(balanceado("".toList))
 
 //Pruebas cambio de monedas
 println(cambioMonedas(4, List(1, 2)))
-println(cambioMonedas(0,List(200,500)))
-println(cambioMonedas(4000,List(200,500)))
-println(cambioMonedas(300, List(5,10,20,50,100,200,500)))
-println(cambioMonedas(301,List(5,10,20,50,100,200,500)))
-
-
-
-
-
-
-
-
-
-
-
-
-
+println(cambioMonedas(0, List(200, 500)))
+println(cambioMonedas(4000, List(200, 500)))
+println(cambioMonedas(300, List(5, 10, 20, 50, 100, 200, 500)))
+println(cambioMonedas(301, List(5, 10, 20, 50, 100, 200, 500)))
 
 /*</script>*/ /*<generated>*//*</generated>*/
 }
